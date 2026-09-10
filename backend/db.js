31dc7186c0f3beca717e26df49abe572a8369f9a
Vispok <visheshpokhriyal598@gmail.com>
2026-09-10 22:45:45 +0530

    Databse Created

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+const {pool} = require('pg');
+require('dotenv').config();
+
+const pool = new pool({
+    user: process.env.DB_USER ,
+    host: process.env.DB_HOST ,
+    database: process.env.DB_NAME ,
+    password: process.env.DB_PASSWORD ,
+    port: process.env.DB_PORT,
+});
+
+pool.on('connect', ()=>{
+    console.log("Database is Connected Successfully");
+});
+
+module.exports = pool;
